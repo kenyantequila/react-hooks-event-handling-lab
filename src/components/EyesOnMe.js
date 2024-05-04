e6e0d1317a7ecdfd588c8bd5073acb0c72f9fa37
kenyantequila <abdishakur431@gmail.com>
2024-05-04 23:04:14 +0300

    commit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+export default function EyesOnMe() {
+  return (
+    <button onBlur = {()=>{
+        console.log('Hey! Eyes on me!')
+    }} onFocus = {()=>{
+        console.log('Good!')
+    }} >Eyes on me</button>
+  )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,13 @@
 // Code Keypad Component Here
-
-function Keypad (){
+function Keypad() {
+    function handleClick() {
+      console.log("Entering password...");
+    }
     return (
-        <div></div>
-    )
-}
-
-export default Keypad;
+      <div>
+        <input type="password" onChange={handleClick} />
+      </div>
+    );
+  }
+  
+  export default Keypad;
